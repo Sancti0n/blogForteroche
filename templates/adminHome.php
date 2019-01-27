b98b9842549b16d215ae52d7de680ed712c1020c
Sanction <sayou.jeremie@gmail.com>
2019-01-27 15:13:29 +0100

    Ajout pages Admin

---
+++ templates/adminHome.php
@@ -0,0 +1,14 @@
+<a href="../public/index.php?route=adminAddArticle">Ajouter un article</a>
+<?php
+foreach ($articles as $article) {
+?>
+    <div>
+        <h2><a href="../public/index.php?route=article&idArt=<?= htmlspecialchars($article->getId());?>"><?= htmlspecialchars($article->getTitle());?></a></h2>
+        <p><?= htmlspecialchars($article->getContent());?></p>
+        <p><?= htmlspecialchars($article->getAuthor());?></p>
+        <p>Créé le : <?= htmlspecialchars($article->getDateAdded());?></p>
+    </div>
+    <br>
+<?php
+}
+?>
---
+++ templates/adminLogin.php
@@ -0,0 +1,15 @@
+<form method="post" action="">
+    <p>
+        <label for="pseudo">Pseudo : </label>
+        <input type="text" name="pseudo" id="pseudo">
+    </p>
+    <p>
+        <label for="motdepasse">Mot de passe : </label>
+        <input type="password" name="motdepasse" id="motdepasse">
+    </p>
+    <p>
+        <input type="submit" value="s'identifier">
+    </p>
+</form>
+
+<?php echo $message ?>
