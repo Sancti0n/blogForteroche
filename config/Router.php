60abaa8dc6628be3acb7d57301457e46a588efbd
Sanction <sayou.jeremie@gmail.com>
2019-02-22 22:48:54 +0100

    Modification router

--- config/Router.php
+++ config/Router.php
@@ -20,9 +20,11 @@ class Router {
 
     public function run() {
         try {
-            if (isset($_GET['route'])) {
-                if ($_GET['route'] === 'article') {
-                    if (isset($_GET['idArt'])) {
+            $route = filter_input(INPUT_GET, 'route', FILTER_SANITIZE_STRING);
+            if ($route) {
+                if (filter_input(INPUT_GET, 'route', FILTER_SANITIZE_STRING) === 'article') {
+                    $idArticle = filter_input(INPUT_GET, 'idArt', FILTER_SANITIZE_NUMBER_INT);
+                    if ($idArticle) {
                         $this->frontController->article(filter_input(INPUT_GET, 'idArt', FILTER_SANITIZE_NUMBER_INT));
                         $_SESSION['route'] = $_SERVER['QUERY_STRING'];
                     }
@@ -30,36 +32,36 @@ class Router {
                         $this->errorController->unknown();
                     }
                 }
-                else if ($_GET['route'] === 'reportComment') {
+                else if (filter_input(INPUT_GET, 'route', FILTER_SANITIZE_STRING) === 'reportComment') {
                     $this->frontController->reportComment(filter_input(INPUT_GET, 'idComment', FILTER_SANITIZE_NUMBER_INT), filter_input(INPUT_GET, 'idArt', FILTER_SANITIZE_NUMBER_INT));
                 }
-                else if ($_GET['route'] === 'postComment') {
+                else if (filter_input(INPUT_GET, 'route', FILTER_SANITIZE_STRING) === 'postComment') {
                     $this->frontController->addCommentsFromForm(filter_input(INPUT_GET, 'idArt', FILTER_SANITIZE_NUMBER_INT), filter_input_array(INPUT_POST));
                 }
-                else if ($_GET['route'] === 'updateComment') {
+                else if (filter_input(INPUT_GET, 'route', FILTER_SANITIZE_STRING) === 'updateComment') {
                     $this->backController->updateComment(filter_input(INPUT_GET, 'idComment', FILTER_SANITIZE_NUMBER_INT));
                 }
-                else if ($_GET['route'] === 'updateArticle') {
+                else if (filter_input(INPUT_GET, 'route', FILTER_SANITIZE_STRING) === 'updateArticle') {
                     $this->backController->updateArticle(filter_input(INPUT_GET, 'idArt', FILTER_SANITIZE_NUMBER_INT));
                 }
-                else if ($_GET['route'] === 'deleteComment') {
+                else if (filter_input(INPUT_GET, 'route', FILTER_SANITIZE_STRING) === 'deleteComment') {
                     $this->backController->deleteComment(filter_input(INPUT_GET, 'idComment', FILTER_SANITIZE_NUMBER_INT));
                 }
-                else if ($_GET['route'] === 'deleteArticle') {
+                else if (filter_input(INPUT_GET, 'route', FILTER_SANITIZE_STRING) === 'deleteArticle') {
                     $this->backController->deleteArticle(filter_input(INPUT_GET, 'idArt', FILTER_SANITIZE_NUMBER_INT));
                     $_SESSION['route'] = $_SERVER['QUERY_STRING'];
                 }
-                else if ($_GET['route'] === 'adminAddArticle') {
+                else if (filter_input(INPUT_GET, 'route', FILTER_SANITIZE_STRING) === 'adminAddArticle') {
                     $this->backController->addArticle(filter_input_array(INPUT_POST));
                 }
-                else if ($_GET['route'] === 'adminHome') {
+                else if (filter_input(INPUT_GET, 'route', FILTER_SANITIZE_STRING) === 'adminHome') {
                     $this->backController->adminHome();
                     $_SESSION['route'] = $_SERVER['QUERY_STRING'];
                 }
-                else if ($_GET['route'] === 'adminDeconnexion') {
+                else if (filter_input(INPUT_GET, 'route', FILTER_SANITIZE_STRING) === 'adminDeconnexion') {
                     $this->backController->adminDeconnexion();
                 }
-                else if ($_GET['route'] === 'adminLogin') {
+                else if (filter_input(INPUT_GET, 'route', FILTER_SANITIZE_STRING) === 'adminLogin') {
                     if (isset($_SESSION['adminIsLoggued'])) {
                         header('Location: ../public/index.php?route=adminHome');
                     }
